59de535d707c80b09708c986c277e2ad685d4d89
Taral Pawar <taralpawar@gmail.com>
2020-06-11 22:41:17 +0530

    Updates

---
+++ precache-manifest.85dcccdc171b4527855233196e43975f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bddccc2eef5a5d9ca376eb2a3bb24ae9",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "48452b0083cacbb645c0",
+    "url": "/react-recipe-app/static/css/main.44375c4f.chunk.css"
+  },
+  {
+    "revision": "9e51a420b2ae8759a83b",
+    "url": "/react-recipe-app/static/js/2.e55f4f7f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-recipe-app/static/js/2.e55f4f7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48452b0083cacbb645c0",
+    "url": "/react-recipe-app/static/js/main.34465099.chunk.js"
+  },
+  {
+    "revision": "d2312a775c6832e68374",
+    "url": "/react-recipe-app/static/js/runtime-main.dd797355.js"
+  }
+]);
